9fc054dc996a0862ff681db05bcbbc0ddfa5f8a8
arya1802 <59442589+arya1802@users.noreply.github.com>
2020-10-10 17:12:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { createSwitchNavigator} from 'react-navigation';
+
+import HomeScreen from './screens/HomeScreen';
+
+export default function App() {
+  return (
+    <HomeScreen/>
+  );
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SURC
+# book-santa-stage-2
+Stage - 2
